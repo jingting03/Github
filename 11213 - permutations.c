296266b8c5a4ee7e82be2615548427cfd529c3f1
jingting03 <114464535+jingting03@users.noreply.github.com>
2022-11-20 23:58:24 +0800

    Update 11213 - permutations.c

--- 11213 - permutations.c
+++ 11213 - permutations.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 void swap(int *p1, int *p2)
 {
+	//t是暫存的部分
 	int t=*p1;
 	*p1=*p2;
 	*p2=t;
  }
  void permutation(int a[], int index, int size)
- {
+ {	//如果已經弄好4個數就開始printf
  	if (index ==size)
  	{
  		for (int i=0; i<size; i++)
@@ -18,6 +19,7 @@ void swap(int *p1, int *p2)
 		for(int j=index; j<size; j++){
 			swap(&a[j],&a[index]);
 			permutation(a,index+1,size);
+			//重啓，找尋第二種排法
 			swap(&a[j],&a[index]);
 		}
 	}
@@ -25,10 +27,13 @@ void swap(int *p1, int *p2)
  int main()
  {
  	int n;
+	//決定要排幾個數字
  	scanf("%d",&n);
  	int a[n];
+	//先確立好原本的a[i]
  	for (int i=0; i<n; i++)
  	a[i] = i+1;
+	//呼叫function
  	permutation(a,0,n);
  	return 0;
  }
