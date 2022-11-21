1520d0015f41b9978cc028aacd240af6de54e2c5
jingting03 <114464535+jingting03@users.noreply.github.com>
2022-11-21 08:14:57 +0800

    Update 12459 - Sierpinski Carpet.c

--- 12459 - Sierpinski Carpet.c
+++ 12459 - Sierpinski Carpet.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 long long int c = 0;
- 
+ //新的方形数量就是上一个的一个+上一个的9个
 int add(int n){
 	int i;
   if(n > 0){
+	 
     c++;
     for(i = 0; i < 8; i++) 
+	    //for 上一个的square加八次
 	add(n-1);
   }
 }
